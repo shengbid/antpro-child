2a8b4eda3d1b5ab1665c1be3270faa373ea5447d
laism <laism@chinacsci.com>
2021-12-16 10:41:48 +0800

    提交qiankun微服务子应用

---
+++ src/pages/form/detail/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import { Descriptions, Card } from 'antd'
+ 
+const Detail: React.FC = () => {
+  return (
+    <div>
+      <Card>
+        <h2>这是子应用详情页</h2>
+        <Descriptions title="用户信息">
+          <Descriptions.Item label="UserName">Zhou Maomao</Descriptions.Item>
+          <Descriptions.Item label="Telephone">1810000000</Descriptions.Item>
+          <Descriptions.Item label="Live">Hangzhou, Zhejiang</Descriptions.Item>
+          <Descriptions.Item label="Remark">empty</Descriptions.Item>
+          <Descriptions.Item label="Address">
+            No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
+          </Descriptions.Item>
+        </Descriptions>
+      </Card>
+    </div>
+  )
+}
+ 
+export default Detail
