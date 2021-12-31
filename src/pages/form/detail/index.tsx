2e3ca06140ab1ce4bb6ba2bae1bb18571db9eb0c
laism <laism@chinacsci.com>
2021-12-31 17:27:36 +0800

    feat: 增加动态主题测试组件

--- src/pages/form/detail/index.tsx
+++ src/pages/form/detail/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
-import { Descriptions, Card } from 'antd'
+import { Descriptions, Card, Button } from 'antd'
+import { GithubOutlined } from '@ant-design/icons'
+import style from './index.less'
  
 const Detail: React.FC = () => {
   return (
@@ -15,6 +17,12 @@ const Detail: React.FC = () => {
             No. 18, Wantang Road, Xihu District, Hangzhou, Zhejiang, China
           </Descriptions.Item>
         </Descriptions>
+        <Button type="primary">
+          提交
+        </Button>
+        <div className={style.back}>
+        <GithubOutlined />自定义样式
+        </div>
       </Card>
     </div>
   )
